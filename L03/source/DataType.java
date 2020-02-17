5d1c132cedefd815990ccd43a62022348fd13eea
GreenColoredLife <greencoloredlife@gmail.com>
2020-02-17 09:12:06 +0700

    L03

---
+++ L03/source/DataType.java
@@ -0,0 +1,17 @@
+import Java101;
+
+public class DataType {
+	public static void main(String args[]) {
+		{
+			System.out.println(20.0/2.5);
+			System.out.println("3"+2+1);
+			System.out.println(5+"6"+7);
+			System.out.println(4/6*3);
+			System.out.println(3.0/4*4);
+			System.out.println((short)1.5);
+			System.out.println(3+2.0F+4.0);
+			System.out.println(4-6==7-9);
+			System.out.println((int)(5-6.5)==1-2);
+		}
+	}
+}
